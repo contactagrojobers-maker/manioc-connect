569f58b4b24b40731788b78881f64a0ede335f85
contactagrojobers-maker <contact.agrojobers@gmail.com>
2026-07-23 18:50:48 +0100

    Add files via upload

--- db.js
+++ db.js
@@ -18,8 +18,8 @@
  * ------------------------------------------------------------
  */
 
-const SUPABASE_URL = 'https://VOTRE-PROJET.supabase.co';
-const SUPABASE_ANON_KEY = 'VOTRE_CLE_ANON_PUBLIC';
+const SUPABASE_URL = 'https://ojxzamzyjfcmuruppbyl.supabase.co/rest/v1/';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9qeHphbXp5amZjbXVydXBwYnlsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM1Mzc0OTcsImV4cCI6MjA5OTExMzQ5N30.uKu_GsJXZVQOunmCoWrkG7sJIMb4-pqas9fn-H-33CA';
 
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
